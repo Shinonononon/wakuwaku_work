c03dca95f67cd2a4bfeeb30588a56b964795eba2
Shinonononon <h1karu.sn0928@gmail.com>
2024-07-05 16:05:42 +0900

    ディペンデントした

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,5 +1,5 @@
 class BlogsController < ApplicationController
-  before_action :set_blog, only: %i[show edit update]
+  before_action :set_blog, only: %i[show edit update destroy]
 
   def index
     @blogs = Blog.all
